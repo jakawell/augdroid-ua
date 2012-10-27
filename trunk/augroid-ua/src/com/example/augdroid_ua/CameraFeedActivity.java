21e24ea4610de28301f92eff196740ab697ef616
jakawell@gmail.com <jakawell@gmail.com@96f784ba-5f76-4361-92a3-1a0fe8713cfc>
2012-10-27 21:20:51 +0000

    Added basic sensor reading so that the app now works as a simple compass. An (really blippy) AR green dot now appears on the screen in the direction of north.
    git-svn-id: file:///home/jesse/downloads/augdroid-ua@8 96f784ba-5f76-4361-92a3-1a0fe8713cfc

--- trunk/augroid-ua/src/com/example/augdroid_ua/CameraFeedActivity.java
+++ trunk/augroid-ua/src/com/example/augdroid_ua/CameraFeedActivity.java
@@ -2,16 +2,30 @@ package com.example.augdroid_ua;
 
 import android.app.Activity;
 import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
-public class CameraFeedActivity extends Activity {
+public class CameraFeedActivity extends Activity implements SensorEventListener {
+	
+	private static final String TAG = "CameraFeedActivity";
 	
 	private Camera mCamera;
 	private CameraFeedView mCameraFeedView;
 	private CameraOverlayView mCameraOverlayView;
 	private FrameLayout mFrame;
+	
+	private SensorManager mSensorManger;
+	private float[] mAccelerometerData = new float[3];
+	private float[] mMagneticData = new float[3];
+	private float[] mRawRotationMatrix = new float[9];
+	private float[] mRemappedRotationMatrix = new float[9];
+	private float[] mOrientation = new float[3];
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +33,8 @@ public class CameraFeedActivity extends Activity {
 		setContentView(R.layout.camera_feed_layout);
 		
 		mFrame = (FrameLayout)findViewById(R.id.camera_feed_preview);
-		// camera and feed are set up in onResume()
+		mSensorManger = (SensorManager)getSystemService(SENSOR_SERVICE);
+		// camera and sensors are set up in onResume()
 	}
 
 	private void setupCamera() {
@@ -48,17 +63,46 @@ public class CameraFeedActivity extends Activity {
 			mCamera = null;
 		}
 	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setupCamera();
+		mSensorManger.registerListener(this, mSensorManger.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_UI);
+		mSensorManger.registerListener(this, mSensorManger.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
+	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
 		releaseCamera();
+		mSensorManger.unregisterListener(this);
 	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		setupCamera();
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		Log.d(TAG, "Accuracy Changed");
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		switch (event.sensor.getType()) {
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			System.arraycopy(event.values, 0, mMagneticData, 0, 3);
+			break;
+		case Sensor.TYPE_ACCELEROMETER:
+			System.arraycopy(event.values, 0, mAccelerometerData, 0, 3);
+			break;
+		}
+		
+		if (SensorManager.getRotationMatrix(mRawRotationMatrix, null, mAccelerometerData, mMagneticData)) {
+			SensorManager.remapCoordinateSystem(mRawRotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, mRemappedRotationMatrix);
+			SensorManager.getOrientation(mRemappedRotationMatrix, mOrientation);
+			if (mCameraOverlayView != null) {
+				mCameraOverlayView.refresh(mOrientation);
+			}
+		}
+		else {
+			Log.d(TAG, "Roatation matrix calculation failed.");
+		}
 	}
 	
 }
--- trunk/augroid-ua/src/com/example/augdroid_ua/CameraOverlayView.java
+++ trunk/augroid-ua/src/com/example/augdroid_ua/CameraOverlayView.java
@@ -10,8 +10,15 @@ public class CameraOverlayView extends View {
 
 	private Paint mPaint = new Paint();
 	
+	private float[] mOrientation = null;
+	
 	public CameraOverlayView(Context context) {
 		super(context);
+	}
+	
+	public void refresh(float[] newOrientation) {
+		mOrientation = newOrientation;
+		invalidate();
 	}
 	
 	@Override
@@ -20,8 +27,20 @@ public class CameraOverlayView extends View {
 		int height = getHeight();
 		int width = getWidth();
 		
-		mPaint.setStyle(Paint.Style.FILL);
-		mPaint.setColor(Color.GREEN);
-		canvas.drawCircle(width / 2, height / 2, 50, mPaint);
+		if (mOrientation != null) {
+//			String testBox = "azimuth:\t " + mOrientation[0] + "\n   pitch:\t " + mOrientation[1] + "\n   roll:\t " + mOrientation[2];
+//			float azimuth = ((float)Math.round(Math.toDegrees(mOrientation[0]) * 2)) / 2.0f;
+			float azimuth = (float)Math.toDegrees(mOrientation[0]);
+			String testBox = "azimuth: " + azimuth;
+			mPaint.setColor(Color.RED);
+			mPaint.setTextSize(24);
+			canvas.drawText(testBox, 30, 30, mPaint);
+			
+			if (azimuth < 40 && azimuth > -40) {
+				mPaint.setStyle(Paint.Style.FILL);
+				mPaint.setColor(Color.GREEN); 
+				canvas.drawCircle(((float)width / 2.0f) - (((azimuth * 2.5f)/100.0f)*((float)width/2.0f)), height / 2, 50, mPaint);
+			}
+		}
 	}
 }
